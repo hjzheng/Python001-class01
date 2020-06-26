26ddc6699cdcb10b15a72257577bd54792a31248
hjzheng <hjzheng@yuansuan.cn>
2020-06-26 22:08:07 +0800

    homework2

--- week01/homework2/maoyan/maoyan/settings.py
+++ week01/homework2/maoyan/maoyan/settings.py
@@ -76,9 +76,9 @@ DOWNLOAD_DELAY = 3
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#    'maoyan.pipelines.MaoyanPipeline': 300,
-# }
+ITEM_PIPELINES = {
+    'maoyan.pipelines.MaoyanPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
